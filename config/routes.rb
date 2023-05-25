f866afcd16143bd32d36d6ea910acacfc70a7488
marc messager <marc.messager@gmail.com>
2023-05-25 16:09:13 -0400

    models ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :movie do
+    resources :lists, :only => [:index, :new]
+    resources :bookmarks, only: :new
+  end
+  resources :bookmarks, only: :delete
+  # resources :prison do
+  #   resources :inmates, only: :create
+  # end
+  # resources :inmates, only: :destroy
 end
